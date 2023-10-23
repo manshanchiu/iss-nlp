46fe8692d5fd930f2d8828f97cfba3709f72a06e
Roy Chiu <manshan.chiu@grabtaxi.com>
2023-10-24 00:50:57 +0800

    fix config

--- conversational/start.sh
+++ conversational/start.sh
@@ -2,7 +2,7 @@ export HOST="127.0.0.1"
 export PORT="8081" 
 
 
-printf "const config = {\"host\":\"$HOST\",\"port\":\"$PORT\"}" > conversationalweb/config.js
+printf "const config = {\"host\":\"$HOST\",\"port\":\"$PORT\"}" > conversational/web/config.js
 
 open conversational/web/index.html
 
